7f72942aee37e7f45698c5377e67fb0afcf3cd0a
Ted Liou <mail@tedliou.com>
2022-03-01 16:20:22 +0800

    BOOM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,29 +13,36 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D Rigidbody2D;
 
     [Header("Stats")]
-    public bool ColliderTrigger;
     public bool Rush;
     public float CurrentRushDistance;
     public Vector2 LastMovement;
     public Vector2 LastPosition;
+    public Vector2 MoveTargetPos;
 
     private void Awake()
     {
         PlayerAction = new PlayerAction();
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        PlayerAction.Player_Map.Rush.started += _ =>
+        {
+            
+        };
         PlayerAction.Player_Map.Rush.performed += _ =>
         {
-            RaycastHit2D hit;
-            if (Physics2D.Raycast(transform.position, LastMovement))
+            var hit = Physics2D.Raycast(transform.position, LastMovement, RushDistance, 1 << 0);
+            if (hit)
             {
-
+                MoveTargetPos = hit.point;
             }
-
-            if (CurrentRushDistance == RushDistance)
+            else
             {
-                Debug.Log("Rush");
-                Rush = true;
+                MoveTargetPos = (Vector2)transform.position + LastMovement.normalized * RushDistance;
             }
+            Rush = true;
+        };
+        PlayerAction.Player_Map.Rush.canceled += _ =>
+        {
+            Rush = false;
         };
         CurrentRushDistance = RushDistance;
     }
@@ -54,19 +61,13 @@ public class PlayerController : MonoBehaviour
     {
         Movement = PlayerAction.Player_Map.Movememet.ReadValue<Vector2>();
 
-        if (Rush && !ColliderTrigger)
+        if (Rush)
         {
+            CurrentRushDistance = Vector2.Distance(transform.position, MoveTargetPos);
             Rigidbody2D.velocity = LastMovement * RushSpeed;
-            CurrentRushDistance = CurrentRushDistance - Vector2.Distance(transform.position, LastPosition);
-            if (CurrentRushDistance <= 0)
-            {
-                CurrentRushDistance = RushDistance;
-                Rush = false;
-            }
         }
         else
         {
-            ColliderTrigger = false;
             Rigidbody2D.velocity = Movement * MoveSpeed;
 
             if (Movement != Vector2.zero)
@@ -85,10 +86,5 @@ public class PlayerController : MonoBehaviour
                 LastPosition = transform.position;
             }
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        ColliderTrigger = true;
     }
 }
