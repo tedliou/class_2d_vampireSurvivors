272162fe4a609bd070ef63d30fd302ad398306dd
Ted Liou <mail@tedliou.com>
2022-03-01 15:33:20 +0800

    Add Player Move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float MoveSpeed;
+    public Vector2 Movement;
+    public PlayerAction PlayerAction;
+    public Rigidbody2D Rigidbody2D;
+
+    private void Awake()
     {
-        
+        PlayerAction = new PlayerAction();
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        PlayerAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        PlayerAction.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement = PlayerAction.Player_Map.Movememet.ReadValue<Vector2>();
+        Rigidbody2D.velocity = Movement * MoveSpeed;
+
+        if (Movement != Vector2.zero)
+        {
+            var scale = transform.localScale;
+            if (Movement.x < 0)
+            {
+                scale.x = -1;
+            }
+            else
+            {
+                scale.x = 1;
+            }
+            transform.localScale = scale;
+        }
     }
 }
